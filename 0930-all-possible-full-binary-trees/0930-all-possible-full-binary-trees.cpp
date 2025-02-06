57e140f4d3a73fb223cb0ef4350f3a355cbcca48
Sujal Bazari <117498738+sujalbazari18@users.noreply.github.com>
2025-02-06 11:10:33 +0530

    Time: 3 ms (60.29%), Space: 31.4 MB (60.77%) - LeetHub

--- 0930-all-possible-full-binary-trees/0930-all-possible-full-binary-trees.cpp
+++ 0930-all-possible-full-binary-trees/0930-all-possible-full-binary-trees.cpp
@@ -11,23 +11,23 @@
  */
 class Solution {
 public:
+        vector<vector<TreeNode*>> dp;
     // void print()
-    vector<TreeNode*> allPossibleFBT(int n) {
+    vector<TreeNode*> allPossibleFBT(int n){
         if(n%2==0)return {};
-        if(n==1)return {new TreeNode(0)};
+        if(dp.empty())dp.resize(n+1);
+        if(!dp[n].empty())return dp[n];
+        if(n==1)return dp[n]={new TreeNode(0)};
         vector<TreeNode*> result;
         for(int i=1;i<n;i+=2){
             vector<TreeNode*> left=allPossibleFBT(i);
             vector<TreeNode*> right=allPossibleFBT(n-1-i);
             for(auto& lefts:left){
                 for(auto& rights:right){
-                    TreeNode* root=new TreeNode(0);
-                    root->left=lefts;
-                    root->right=rights;
-                    result.push_back(root);
+                    result.push_back(new TreeNode(0,lefts,rights));
                 }
             }
         }
-        return result;
+        return dp[n]=result;
     }
 };
