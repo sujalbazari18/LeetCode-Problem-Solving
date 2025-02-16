fb67319df6a00d58f53fd48481507341599fbccd
Sujal Bazari <117498738+sujalbazari18@users.noreply.github.com>
2025-02-16 16:52:37 +0530

    Time: 0 ms (100%), Space: 8.6 MB (100%) - LeetHub

---
+++ 3709-find-special-substring-of-length-k/3709-find-special-substring-of-length-k.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasSpecialSubstring(string s, int k) {
+        int n=s.length();
+        int i=0;
+        for(int j=0;j<n;j++) {
+            if(s[j]==s[i])continue;
+            if(j-i==k)return true;
+            i=j;
+        }
+        return (n-i)==k;
+    }
+};
