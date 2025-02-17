23587b5b6cd2988e26d740cd48e2d63deb5cb867
Sujal Bazari <117498738+sujalbazari18@users.noreply.github.com>
2025-02-17 10:29:48 +0530

    Time: 3 ms (78.62%), Space: 7.9 MB (84.16%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -2,11 +2,24 @@ class Solution {
 public:
     int numTilePossibilities(string tiles) {
         int n=tiles.size();
-        unordered_map<int,int> f;
-        for(auto& tile:tiles)f[tile]++;
-        return fun(f);
+        // map<char,int> f;
+        vector<int> arr(26,0);
+        for(auto& a: tiles)arr[a-'A']++;
+        // for(auto& tile:tiles)f[tile]++;
+        return fun(arr);
     }
-    int fun(unordered_map<int,int>& freq){
+    int fun(vector<int>& arr){
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(arr[i]>0){
+                arr[i]--;
+                count+=1+fun(arr);
+                arr[i]++;
+            }
+        }
+        return count;
+    }
+    int fun(map<char,int>& freq){
         int count=0;
         for(auto& [a,b]:freq){
             if(b>0){
@@ -18,3 +31,5 @@ public:
         return count;
     }
 };
+
+
