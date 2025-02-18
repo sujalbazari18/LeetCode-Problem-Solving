f7cf520da54de86ed66cb04fd2288b94d3406dce
Sujal Bazari <117498738+sujalbazari18@users.noreply.github.com>
2025-02-18 11:58:21 +0530

    Time: 0 ms (100%), Space: 11.9 MB (6.7%) - LeetHub

--- 0979-di-string-match/0979-di-string-match.cpp
+++ 0979-di-string-match/0979-di-string-match.cpp
@@ -5,11 +5,9 @@ public:
         int i=0,j=s.size();
         for(char c:s){
             if(c=='I'){
-                ans.push_back(i);
-                i++;
+                ans.push_back(i++);
             }else{
-                ans.push_back(j);
-                j--;
+                ans.push_back(j--);
             }
         }
         ans.push_back(i);
