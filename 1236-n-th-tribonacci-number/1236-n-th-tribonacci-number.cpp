c6c47b7e384deea794f5abd5cbb094c579187e4c
Sujal Bazari <117498738+sujalbazari18@users.noreply.github.com>
2025-03-08 16:50:09 +0530

    Time: 0 ms (100%), Space: 8.6 MB (6.38%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fun(int n,vector<int>& dp) {
+        if(n==0)return 0;
+        if(n==1)return 1;
+        if(n==2)return 1;
+        if(dp[n]!=-1)return dp[n];
+        return dp[n]=fun(n-1,dp)+fun(n-2,dp)+fun(n-3,dp);
+    }
+    int tribonacci(int n){
+        vector<int> dp(n+1,-1);
+        return fun(n,dp);
+    }
+};
