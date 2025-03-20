739baf28f66d9ec42e4102d3d541ca37bc396b54
Sujal Bazari <117498738+sujalbazari18@users.noreply.github.com>
2025-03-20 10:33:37 +0530

    Time: 11 ms (6.07%), Space: 59.4 MB (26.23%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
+    bool isValid(int mid,int k,vector<int>& nums){
+        int zeros=0;
+        for(int i=0;i<mid;i++){
+            if(nums[i]==0)zeros++;
+        }
+        if(zeros<=k)return true;
+        for(int i=mid;i<nums.size();i++){
+            if(nums[i-mid]==0)zeros--;
+            if(nums[i]==0)zeros++;
+            if(zeros<=k)return true;
+        }
+        return false;
+    }
     int longestOnes(vector<int>& nums, int k) {
         int n=nums.size();
-        int a=0;
-        int b=0,ans=0;
-        for(int i=0;i<n;i++){
-            // if(nums[i]==1)ans++;
-            if(nums[i]==0){
-                a++;
-            }
-            while(a>k){
-                if(nums[b]==0){
-                    a--;
-                }
-                b++;
-            }
-            ans=max(ans,i-b+1);
+        // int a=0;
+        // int b=0,ans=0;
+        // for(int i=0;i<n;i++){
+        //     // if(nums[i]==1)ans++;
+        //     if(nums[i]==0){
+        //         a++;
+        //     }
+        //     while(a>k){
+        //         if(nums[b]==0){
+        //             a--;
+        //         }
+        //         b++;
+        //     }
+        //     ans=max(ans,i-b+1);
+        // }
+        // return ans;
+        int left=0,right=n,ans=0;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(isValid(mid,k,nums)){
+                ans=mid;
+                left=mid+1;
+            }else right=mid-1;
         }
         return ans;
     }
