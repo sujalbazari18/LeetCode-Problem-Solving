447391d225d599f143fdd6a893dbd2d3c4ac7cd1
Sujal Bazari <117498738+sujalbazari18@users.noreply.github.com>
2025-03-25 18:19:59 +0530

    Time: 36 ms (33.32%), Space: 23.7 MB (52.55%) - LeetHub

---
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        if(word1.size()!=word2.size())return false;
+        vector<int> f(26,0);
+        vector<int> s(26,0);
+        unordered_set<char> set1,set2;
+        for(int i=0;i<word1.size();i++){
+            f[word1[i]-'a']++;
+            set1.insert(word1[i]);
+        }
+        for(int i=0;i<word2.size();i++){
+            s[word2[i]-'a']++;
+            set2.insert(word2[i]);
+        }
+        if(set1!=set2)return false;
+        sort(f.begin(),f.end());
+        sort(s.begin(),s.end());
+        return s==f;
+    }
+};
