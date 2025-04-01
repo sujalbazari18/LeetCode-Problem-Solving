33dab1623c3d9f3f6b88407d3832e588b546b496
Sujal Bazari <117498738+sujalbazari18@users.noreply.github.com>
2025-04-01 11:59:16 +0530

    Time: 8 ms (49.73%), Space: 127.6 MB (27.26%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,23 +1,23 @@
+#include <vector>
+#include <cstring>
+using namespace std;
+
 class Solution {
+private:
+    long rec(int i, vector<vector<int>>& questions, vector<long>& dp, int n) {
+        if (i >= n) return 0;
+        if (dp[i] != -1) return dp[i];
+
+        long take = questions[i][0] + rec(i + questions[i][1] + 1, questions, dp, n);
+        long dont = rec(i + 1, questions, dp, n);
+
+        return dp[i] = max(take, dont);
+    }
+
 public:
-    // long long fun(int i,vector<vector<int>> q,vector<long long>& dp){
-    //     if(i>=q.size())return 0;
-    //     if(dp[i]!=-1)return dp[i];
-    //     long long skip=fun(i+1,q,dp);
-    //     long long take=q[i][0]+fun(i+q[i][1]+1,q,dp);
-    //     return dp[i]=max(skip,take);
-    // }
-    long long mostPoints(vector<vector<int>>& questions) {
-        vector<long long> dp(questions.size()+1,0);
-        int n=questions.size();
-        for(int i=0;i<n;i++){
-            if(i>0)dp[i]=max(dp[i],dp[i-1]);
-            int next=i+questions[i][1]+1;
-            int take=questions[i][0];
-            if(next<n)dp[next]=max(dp[next],dp[i]+take);
-            else
-            dp[n]=max(dp[n],dp[i]+take);
-        }
-        return max(dp[n],dp[n-1]);
+    long mostPoints(vector<vector<int>>& questions) {
+        int n = questions.size();
+        vector<long> dp(n, -1);
+        return rec(0, questions, dp, n);
     }
 };
