af59bba694d4676f73fe1af07597b933257b7ff6
Sujal Bazari <117498738+sujalbazari18@users.noreply.github.com>
2025-04-01 12:08:26 +0530

    Time: 11 ms (32.59%), Space: 127.6 MB (27.26%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,23 +1,16 @@
-#include <vector>
-#include <cstring>
-using namespace std;
-
 class Solution {
-private:
-    long rec(int i, vector<vector<int>>& questions, vector<long>& dp, int n) {
-        if (i >= n) return 0;
-        if (dp[i] != -1) return dp[i];
-
-        long take = questions[i][0] + rec(i + questions[i][1] + 1, questions, dp, n);
-        long dont = rec(i + 1, questions, dp, n);
-
-        return dp[i] = max(take, dont);
-    }
-
 public:
+    long fun(int i,vector<vector<int>>& q,vector<long>& dp,int n){
+        if(i>=n)return 0;
+        if(dp[i]!=-1)return dp[i];
+        long take=q[i][0]+fun(i+q[i][1]+1,q,dp,n);
+        long skip=fun(i+1,q,dp,n);
+        return dp[i]=max(skip,take);
+
+    }
     long mostPoints(vector<vector<int>>& questions) {
-        int n = questions.size();
-        vector<long> dp(n, -1);
-        return rec(0, questions, dp, n);
+        int n=questions.size();
+        vector<long> dp(n,-1);
+        return fun(0,questions,dp,n);
     }
 };
