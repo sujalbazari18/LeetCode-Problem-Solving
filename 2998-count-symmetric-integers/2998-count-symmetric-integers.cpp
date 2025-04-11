0324468c98e8d6950a4042fc64b76d1a249eed28
Sujal Bazari <117498738+sujalbazari18@users.noreply.github.com>
2025-04-11 18:11:57 +0530

    Time: 66 ms (66.07%), Space: 9.7 MB (74.11%) - LeetHub

---
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countSymmetricIntegers(int low, int high) {
+        int ans=0;
+        for(int i=low;i<=high;i++){
+            string s=to_string(i);
+            if(s.size()%2==0 && accumulate(s.begin(),s.begin()+s.size()/2,0)==accumulate(s.begin()+s.size()/2,s.end(),0))ans++;
+
+        }
+        return ans;
+    }
+};
