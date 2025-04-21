1670ab7ac1cac4712a7147ba34c3a15dc4b665a0
Sujal Bazari <117498738+sujalbazari18@users.noreply.github.com>
2025-04-21 15:38:44 +0530

    Time: 0 ms (100%), Space: 8 MB (75.63%) - LeetHub

--- 2397-count-number-of-ways-to-place-houses/2397-count-number-of-ways-to-place-houses.cpp
+++ 2397-count-number-of-ways-to-place-houses/2397-count-number-of-ways-to-place-houses.cpp
@@ -3,12 +3,13 @@ public:
     int m=1e9+7;
     int countHousePlacements(int n) {
         if(n==1)return 4;
-        vector<int> v(n);
-        v[0]=2;
-        v[1]=3;
-        for(int i=2;i<n;i++){
-            v[i]=(v[i-1]+v[i-2])%m;
+        int a=2;
+        int b=3;
+        for(int i=0;i<n-2;i++){
+            int c=(a+b)%m;
+            a=b;
+            b=c;
         }
-        return (1LL*v[n-1]*v[n-1])%m;
+        return (1LL*b*b)%m;
     }
 };
